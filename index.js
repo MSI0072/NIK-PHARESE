98a4821c42f92212d020d25df964ff127759790e
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2020-12-22 20:51:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var nik = require("nik-parse-api");
+var kontol = require("nik-parse-api");
 
-app.get('/api/nik/:nik', function (req, res) {
-    const nik = req.params["nik"];
-    res.send(nik.nikParse(nik));
+app.get('/api/nik/:cok', function (req, res) {
+    const nik = req.params["cok"];
+    res.send(kontol.nikParse(nik));
   });
   
 //ex : https://your-domain/api/nik/3204110609970001 
